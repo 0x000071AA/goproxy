22fcd398b1ffdfb957ed905c95be36ac7139a512
Tim Weber <Tim1991-Weber@web.de>
2020-03-06 15:28:19 +0100

    added gorilla/mux

--- proxy.go
+++ proxy.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	var targets []*url.URL
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/")
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode()
+	})
+
+	r.HandleFunc("/users").Methods("GET")
+	r.HandleFunc("/users/{user}").Methods("POST")
+	r.HandleFunc("/users/{user}").Methods("PUT")
+	r.HandleFunc("/users/{user}").Methods("DELETE")
 
 	for _, host := range []string{"http://127.0.0.1:8080"} {
 		rpURL, err := url.Parse(host)
